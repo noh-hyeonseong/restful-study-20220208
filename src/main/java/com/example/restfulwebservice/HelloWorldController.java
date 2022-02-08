32c08c247da686914349f42c879eecd67f365e81
noh-hyeonseong <shgustjd6262@naver.com>
2022-02-08 20:39:58 +0900

    주석추가

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -14,11 +14,15 @@ public class HelloWorldController {
         return "I'm 노반장";
     }
 
+    //GET
+    // @bean을 활용한 api
     @GetMapping(path="/noh-world-bean")
     public NohWorldBean NohWorldBean() {
         return new NohWorldBean("I'm 노반장");
     }
 
+    //GET
+    //
     @GetMapping(path="/noh-world-bean/path-variable/{id}")
     public NohWorldBean NohWorldBean(@PathVariable(value = "id") String name) {       //@PathVariable 을 설정한 매개변수를 url path의 변수{name}으로 연결해줌. 만약 id->name변경시 (value = "id") 생략가능
         return new NohWorldBean(String.format("I'm 노반장, %s", name));
