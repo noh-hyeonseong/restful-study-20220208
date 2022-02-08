058242af44a2d187e8068a51eb2ce8490cdb4fbb
noh-hyeonseong <shgustjd6262@naver.com>
2022-02-08 23:18:04 +0900

    add UserController

--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -1,24 +1,28 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class UserDaoService {
-    private static List<User> users = new ArrayList<>();
 
+    private static List<User> users = new ArrayList<>();
+    private int userCount = 3;
     static {
         users.add(new User(1,"noh", new Date()));
         users.add(new User(2,"choi", new Date()));
         users.add(new User(3,"kim", new Date()));
     }
 
-    private static int userCount = 3;
-
     public User save(User user){
         if (user.getId() == null){
             user.setId(++userCount);
         }
+        users.add(user);
+        return user;
     }
 
     public User findOne(int id){
@@ -28,5 +32,9 @@ public class UserDaoService {
             }
         }
         return null;
+    }
+
+    public List<User> findAll() {
+        return users;
     }
 }
