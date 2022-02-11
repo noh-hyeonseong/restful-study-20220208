9efd631f4610217102bf95f5b04fa9b00c00ef4b
noh-hyeonseong <shgustjd6262@naver.com>
2022-02-12 00:16:06 +0900

    add api info

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -4,6 +4,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,14 +19,18 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min=2, message = "name은 2글자 이상입니다.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해 주세요.")
     private Date joinDate;
-
+    @ApiModelProperty(notes = "사용자의 비밀번호를 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요.")
     private String ssn;
 }
