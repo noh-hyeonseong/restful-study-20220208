114c134a01c6acada4b7571944fc198fe335380d
noh-hyeonseong <shgustjd6262@naver.com>
2022-02-12 18:29:27 +0900

    add jpa controller, UserRepository

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
-insert into user values(1, sysdate(), 'sdfg', '1234', '121212-1212121');
+insert into user values(1, sysdate(), '노현성', '1234', '121212-1212121');
 insert into user values(2, sysdate(), 'fhhwe', '1234', '121212-1212121');
 insert into user values(3, sysdate(), 'egq2', '1234', '121212-1212121');
 insert into user values(4, sysdate(), 'gk3k1', '1234', '121212-1212121');
+
+// ' 대신에 " 쓰면 에러남
